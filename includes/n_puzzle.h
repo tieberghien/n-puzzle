342ff392576ce130473b4d6e0f9cbe5ccf07a4f3
Estelle TIEBERGHIEN <etieberg@e1r11p16.42.fr>
2019-09-08 22:00:58 +0200

    test struct

--- includes/n_puzzle.h
+++ includes/n_puzzle.h
@@ -6,7 +6,7 @@
 /*   By: etieberg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/06 14:57:28 by etieberg          #+#    #+#             */
-/*   Updated: 2019/09/08 21:07:53 by etieberg         ###   ########.fr       */
+/*   Updated: 2019/09/08 22:00:41 by etieberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 # define USAGE "Usage: ./solver <sourcefile>"
 # include <fcntl.h>
 # include "libft.h"
+
+typedef struct	s_puzzle
+{
+	size_t		n;
+	int			puzzle[n*n];
+}				t_puzzle;
 
 int read_file(int fd);
 
