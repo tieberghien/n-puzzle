d49a0953d4349e0c97dff9376a979eae549efec4
Estelle TIEBERGHIEN <etieberg@e1r11p16.42.fr>
2019-09-08 22:14:59 +0200

    test struct

--- includes/n_puzzle.h
+++ includes/n_puzzle.h
@@ -6,7 +6,7 @@
 /*   By: etieberg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/06 14:57:28 by etieberg          #+#    #+#             */
-/*   Updated: 2019/09/08 22:00:41 by etieberg         ###   ########.fr       */
+/*   Updated: 2019/09/08 22:14:53 by etieberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,15 @@ typedef struct	s_puzzle
 	size_t		n;
 	int			puzzle[n*n];
 }				t_puzzle;
+
+typedef struct	s_sets
+{
+	t_node		*open;
+	t_node		*closed;
+	size_t		depth;
+	size_t		value;
+	size_t		index;
+}				t_sets;
 
 int read_file(int fd);
 
